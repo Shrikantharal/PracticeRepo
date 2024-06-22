74918e604814f99196481673309b5cc2905ef575
Shrikantharal <shrikantharal@gmail.com>
2024-06-22 22:11:53 +0530

    Commit from Feature One

--- SeleniumLearning/first.cs
+++ SeleniumLearning/first.cs
@@ -7,5 +7,7 @@ public class Class1
 		Console.WriteLine("First");
 		Console.WriteLine("Changes made from feature branch one");
 		Console.WriteLine("Changes made by FeatureOne after commiting feature two");
+
+		Console.WriteLine("Changes made at fetaure One");
 	}
 }
